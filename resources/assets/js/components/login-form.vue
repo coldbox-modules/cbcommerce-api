d88957d463b23072af587371ab73ead06ab3ae28
Esmeralda Acevedo <esme@ortussolutions.com>
2018-12-19 07:49:32 -0800

    Login Form component and supporting files.

---
+++ resources/assets/js/components/login-form.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="login-box block-form">
+		<h2>Sign In to your BSR Online Account</h2>
+		<div class="login-box-body ">
+			<div class="form-group">
+				<div><label for="username">Username:</label></div>
+				<input type="text" name="username"  v-model="username" class="form-control" autocomplete="false" />
+
+				<div><label for="username">Password:</label></div>
+				<input type="password" name="password" v-model="password" class="form-control" autocomplete="false"/>
+				<div class="clearfix"></div>
+			</div>
+			<div><a href="#">Forgot your password?</a></div>
+			<div class="form-group">
+				<button class="btn btn-secondary btn-lg">Login</button>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+	            username: "",
+	            password: ""
+	        }
+		}
+	}
+</script>
---
+++ views/checkout/login.cfm
@@ -0,0 +1,19 @@
+<cfoutput>
+    <section>
+
+        <div class="container-fluid">
+        	<div class="header-for-light">
+            	<h1>Secure <span>Checkout</span></h1>
+            </div>
+            <div class="col-sm-6">
+            	<login-form></login-form>
+            </div>
+            <div class="col-sm-6">
+            	<h2>No BSR Online Account?</h2>
+            	<button class="btn btn-secondary btn-lg">Checkout as a guest</button>
+            	<button class="btn btn-primary btn-lg">Create an Account</button>
+            </div>
+        </div>
+
+    </section>
+</cfoutput>
