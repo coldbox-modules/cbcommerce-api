b8b1b58062f16d0088f093548e490fda2edfd0de
Abilio Posada <aposada@ortussolutions.com>
2020-01-22 08:41:25 -0600

    Update general admin static text references

--- resources/assets/js/admin/components/ui/sidebar.vue
+++ resources/assets/js/admin/components/ui/sidebar.vue
@@ -9,7 +9,7 @@
 				<li class="nav-item">
 					<router-link 
 						class="nav-link" 
-						to="/"><i class="fa fa-dashboard"></i> Dashboard</router-link>
+						to="/"><i class="fa fa-dashboard"></i> {{ $t( 'setting_dashboard' ) }}</router-link>
 				</li>
 
 				<li class="nav-item">
@@ -21,26 +21,26 @@
 				<li class="nav-item">
 					<router-link 
 						class="nav-link" 
-						to="/categories"><i class="fa fa-sitemap"></i> Categories</router-link>
+						to="/categories"><i class="fa fa-sitemap"></i> {{ $t( 'categories' ) }}</router-link>
 				</li>
 
 				<li class="nav-item">
 					<router-link 
 						class="nav-link" 
-						to="/products"><i class="fa fa-shopping-cart"></i> Products</router-link>
+						to="/products"><i class="fa fa-shopping-cart"></i> {{ $t( 'products' ) }}</router-link>
 				</li>
 
 				<li class="nav-item">
 					<router-link 
 						class="nav-link" 
-						to="/customers"><i class="fa fa-users"></i> Customers</router-link>
+						to="/customers"><i class="fa fa-users"></i> {{ $t( 'customers' )}}</router-link>
 				</li>
 
     		</ul>
 
 			<h6 
 				class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-				<span>Settings</span>
+				<span>{{ $t( 'settings' ) }}</span>
 			</h6>
 
 
@@ -49,7 +49,7 @@
 				<li class="nav-item">
 					<router-link
 						class="nav-link"
-						to="/settings/stripe"><i class="fa fa-money"></i> Stripe Configuration</router-link>
+						to="/settings/stripe"><i class="fa fa-money"></i> {{ $t( 'setting_stripe' ) }}</router-link>
 				</li>
 
 			</ul>
